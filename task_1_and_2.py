7b1eb8c89b6c5812d082c1b265806137eabda52d
Abel2333 <abelsparda@outlook.com>
2023-11-08 20:26:02 +0000

    improve the structure

---
+++ task_1_and_2.py
@@ -0,0 +1,31 @@
+from util.fir_filter import FIRDesign, FIRFilter
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    ecg_signal = np.loadtxt("data/ECG_1000Hz_7.dat")
+    filter_design = FIRDesign(1000)
+    coeff_highpass = filter_design.highpass_design(
+        high_frequency=1, frequency_resulotion=0.5
+    )
+    coeff_bandstop = filter_design.bandstopDesign(
+        low_frequency=49, high_frequency=51, frequency_resulotion=0.5
+    )
+
+    filter = FIRFilter(coeff_highpass)
+    filter.add_coefficients(coeff_bandstop)
+    filter.synthesis()
+
+    new_signal = np.zeros(len(ecg_signal))
+
+    for i in range(0, len(ecg_signal)):
+        new_signal[i] = filter.dofilter(ecg_signal[i])
+
+    time = np.arange(0, len(new_signal)) / 1000
+    plt.plot(time, new_signal)
+    plt.savefig("./figures.jpeg")
+
+
+if __name__ == "__main__":
+    main()
---
+++ util/fir_filter.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+class FIRDesign:
+    def __init__(self, _sample) -> None:
+        self.sample = _sample
+    
+    def highpass_design(self, high_frequency, frequency_resulotion) -> np.array:
+        M = int(self.sample / frequency_resulotion)
+
+        # Generate the ideal filter, from frequency domain to time domain
+        ideal_filter = np.zeros(M)
+        highband = int(high_frequency/self.sample * M)
+        ideal_filter[highband:M-highband+1] = 1
+        ideal_filter = np.fft.ifft(ideal_filter).real
+        
+        # Generate actually filter coefficients
+        filter_coefficients = np.empty(M)
+        filter_coefficients[0:int(M/2)] = ideal_filter[int(M/2):M]
+        filter_coefficients[int(M/2):M] = ideal_filter[0:int(M/2)]
+        filter_coefficients = filter_coefficients * np.hamming(M)
+        return filter_coefficients
+    
+    def bandstopDesign(self, low_frequency, high_frequency, frequency_resulotion) -> np.array:
+        M = int(self.sample / frequency_resulotion)
+        
+        # Generate the ideal filter, from frequency domain to time domain
+        ideal_filter = np.ones(M)
+        low_band = int(low_frequency/self.sample * M)
+        high_band = int(high_frequency/self.sample * M)
+        ideal_filter[low_band:high_band+1] = 0
+        ideal_filter[M-high_band:M-low_band+1] = 0
+        ideal_filter = np.fft.ifft(ideal_filter).real
+
+        # Generate actually filter coefficients
+        filter_coefficients = np.empty(M)
+        filter_coefficients[0:int(M/2)] = ideal_filter[int(M/2):M]
+        filter_coefficients[int(M/2):M] = ideal_filter[0:int(M/2)]
+        filter_coefficients = filter_coefficients * np.hamming(M)
+        return filter_coefficients
+    
+
+class FIRFilter:
+    def __init__(self, _coefficients:list):
+        # List can contain many lists with different length
+        self.coefficients_list = [_coefficients]
+        self.real_coefficients = _coefficients
+        self.buffer = np.zeros(len(self.real_coefficients))
+
+    def add_coefficients(self, _coefficients:list):
+        self.coefficients_list.append(_coefficients)
+
+    def synthesis(self):
+        # Get real coefficients from list
+        if len(self.coefficients_list) == 1:
+            return
+        tmp = self.coefficients_list[0]
+        for coeff in self.coefficients_list[1::]:
+            tmp = self.convolution(coeff, tmp)
+        self.real_coefficients = tmp
+        self.buffer = np.zeros(len(self.real_coefficients))
+
+
+    def convolution(self, x_1:list, x_2:list):
+        n = len(x_1)
+        m = len(x_2)
+        x_1 = np.array(x_1)
+        x_2 = np.array(x_2)
+        tmp = np.zeros(2*n+m)
+        tmp[n-1:n+m-1] = x_2
+        result = np.empty(m+n-1)
+        for i in range(0, m+n-1):
+            result[i] = np.dot(x_1, tmp[i:i+n][::-1])
+        return result
+
+
+    def dofilter(self, v):
+        # Shift values in the buffer
+        self.buffer[1:] = self.buffer[:-1]
+        self.buffer[0] = v
+
+        # Apply the filter
+        result = np.dot(self.real_coefficients, self.buffer)
+
+        return result
+    
